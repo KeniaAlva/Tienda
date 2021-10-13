e8600c0e3dd04d7902ef34ca883c31cafe4473bc
DianaPalacios03 <dapalacios@unah.hn>
2021-10-12 19:12:23 -0600

    Segunda parte

--- win.Tienda/FrmPagos.cs
+++ win.Tienda/FrmPagos.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace win.Tienda
 {
-    public partial class Form1 : Form
+    public partial class FrmPagos : Form
     {
-        public Form1()
+        public FrmPagos()
         {
             InitializeComponent();
         }
